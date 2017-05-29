469b538f3879c2de111b5aee8a387032126a4526
kokoa0429 <taitokokoa@gmail.com>
2017-05-29 16:43:51 +0900

    ステージのjson取得の更新

--- app/src/main/java/kokoakobato/myapplication/StageActivity.java
+++ app/src/main/java/kokoakobato/myapplication/StageActivity.java
@@ -13,8 +13,6 @@ public class StageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
